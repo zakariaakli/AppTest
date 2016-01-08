33a3827de408edaffae32e10a350db6494ffab56
zakariaakli <zakaria.akli.ensa@gmail.com>
2016-01-08 01:06:39 +0100

    UPD : Person

---
+++ DAL/Model1.Designer.cs
@@ -0,0 +1,4 @@
+﻿// La génération de code par défaut est désactivée pour le modèle 'C:\Users\thecorner\documents\visual studio 2012\Projects\AppTest\DAL\Model1.edmx'. 
+// Pour l'activer, modifiez la valeur du concepteur 'Stratégie de génération de code'.
+// Cette propriété est disponible dans la fenêtre Propriétés, lorsque le modèle est ouvert
+// dans le concepteur.
